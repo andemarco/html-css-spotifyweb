c6da7fe1392974432e46d678bb4bcf14b68b1674
Andrea De Marco <andreademarco91@gmail.com>
2019-12-17 14:40:07 +0100

    Finiture

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 alert('Heavy Metal diventa Makai');
 document.getElementById('NomeArtista').innerHTML='Makai';
 document.getElementById('TitoloPezzo').innerHTML='Lazy Days';
+document.getElementById("CoverPezzo").src="img/makai.jpeg";
